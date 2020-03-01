e237f7db1b5418f444f4da37f97d92ba141ff51b
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-02-29 21:28:36 -0400

    list of clients

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -2,7 +2,7 @@ const controller = {};
 
 controller.list = (req, res) => {
   req.getConnection((err, conn) => {
-    conn.query("SELECT * FROM client", (err, client) => {
+    conn.query('select a.id_client, b.fathers_last_name,b.mothers_last_name,b.first_name, a.cellphone_number,a.date_of_birth,a.address,a.registation_date from client a, person b where a.id_person=b.id_person', (err, client) => {
       if (err) {
         res.json(err);
       } else {
@@ -14,5 +14,34 @@ controller.list = (req, res) => {
     });
   });
 };
+controller.save=(req,res)=>{
+  const data = req.body;
+  req.getConnection((err,conn)=>{
+    conn.query('INSERT INTO client SET ?',[data],(err,client)=>{
+      res.redirect("/");
+    });
+  })
+};
+controller.edit=(req,res)=>{
+  const { id }=req.params;
+ req.getConnection((err,conn)=>{
+  conn.query('SELECT * FROM client wWHERE id =?',[id],(err,client)=>{
+    res.render('',{
+      data: client[0]
+    });
+  });
+ });
+}
+
+controller.update=(req,res)=>{
+  const {id} = req.params;
+  const new_client = req.body;
+  req.getConnection((err,conn)=>{
+    conn.query('UPDATE customer set ? where id = ?',[newCustomer,id],(err,rows)=>{
+      res.redirect('/');
+  });
+})
+}
+
 
   module.exports = controller;
