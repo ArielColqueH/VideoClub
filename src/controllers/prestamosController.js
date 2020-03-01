afe9d332f6d9516f400b3400f4f8f48f3f4749ed
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-03-01 02:06:38 -0400

    movie list

--- src/controllers/prestamosController.js
+++ src/controllers/prestamosController.js
@@ -2,7 +2,7 @@ const controller = {};
 
 controller.list = (req, res) => {
   req.getConnection((err, conn) => {
-    conn.query("SELECT * FROM video", (err, video) => {
+    conn.query('select a.id_video, b.video_title, c.genre, d.first_name,d.last_name, a.stock from video a, video_title b, genre c, actor d, video_genre e, video_actor f where a.id_video = b.id_video and a.id_video = e.id_video and e.id_genre = c.id_genre and a.id_video = f.id_video and f.id_actor = d.id_actor group by (a.id_video);', (err, video) => {
       if (err) {
         res.json(err);
       } else {
