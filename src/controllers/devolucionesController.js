fc257ce2dd8bedf72f73eb90b1cb93fd6e9c7034
ariel colque <ariel.colque.herrera@gmail.com>
2020-03-04 17:21:50 -0400

    devoluciones search added

--- src/controllers/devolucionesController.js
+++ src/controllers/devolucionesController.js
@@ -1,13 +1,23 @@
 const controller = {};
+var devoluciones =
+  "select a.id_rental,a.start_date,a.devolution_date,c.fathers_last_name,c.mothers_last_name,c.first_name,d.status_name " +
+  "from rental a,client b, person c, rental_status d " +
+  "where b.id_person = c.id_person " +
+  "and b.id_client = a.id_client " +
+  "and a.id_rental_status=d.id_rental_status " +
+  "order by(a.id_rental) asc";
+
+var devolucionesSearch =
+  "select a.id_rental,a.start_date,a.devolution_date,c.fathers_last_name,c.mothers_last_name,c.first_name,d.status_name " +
+  "from rental a,client b, person c, rental_status d " +
+  "where b.id_person = c.id_person " +
+  "and b.id_client = a.id_client " +
+  "and a.id_rental_status=d.id_rental_status and a.id_rental=?" +
+  "order by(a.id_rental) asc";
 
 controller.list = (req, res) => {
   req.getConnection((err, conn) => {
-    conn.query('select a.id_rental,a.start_date,a.devolution_date,c.fathers_last_name,c.mothers_last_name,c.first_name,d.status_name '
-    +'from rental a,client b, person c, rental_status d '+
-    'where b.id_person = c.id_person '+
-    'and b.id_client = a.id_client '+
-    'and a.id_rental_status=d.id_rental_status '+
-    'order by(a.id_rental) asc', (err, rental) => {
+    conn.query(devoluciones, (err, rental) => {
       if (err) {
         res.json(err);
       } else {
@@ -20,4 +30,34 @@ controller.list = (req, res) => {
   });
 };
 
-  module.exports = controller;
+controller.search = (req, res) => {
+  var word = req.body.word;
+  console.log(word);
+  req.getConnection((err, conn) => {
+    if (word) {
+      conn.query(devolucionesSearch, [word], (err, video) => {
+        if (err) {
+          res.json(err);
+        } else {
+          res.render("devoluciones", {
+            data: video
+          });
+        }
+      });
+    } else {
+      if (word == "") {
+        conn.query(devoluciones, (err, video) => {
+          if (err) {
+            res.json(err);
+          } else {
+            res.render("devoluciones", {
+              data: video,
+            });
+          }
+        });
+      }
+    }
+  });
+};
+
+module.exports = controller;
--- src/routes/devolucion.js
+++ src/routes/devolucion.js
@@ -4,5 +4,6 @@ const router = express.Router();
 const devolucionesController = require("../controllers/devolucionesController");
 
 router.get("/", devolucionesController.list);
+router.post("/searchWord", devolucionesController.search);
 
 module.exports = router;
