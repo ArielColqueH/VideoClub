c5800ee562353b5b09b7651b7221a9fc0c480fd5
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-03-04 18:10:15 -0400

    rental list

--- src/controllers/devolucionesController.js
+++ src/controllers/devolucionesController.js
@@ -5,6 +5,7 @@ var devoluciones =
   "where b.id_person = c.id_person " +
   "and b.id_client = a.id_client " +
   "and a.id_rental_status=d.id_rental_status " +
+  "and (d.id_rental_status= 1 or d.id_rental_status=2) "+
   "order by(a.id_rental) asc";
 
 var devolucionesSearch =
