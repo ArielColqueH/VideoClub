6c6536edfccdae5dc4c502847d59eca7100cfec1
ariel colque <ariel.colque.herrera@gmail.com>
2020-03-04 20:33:25 -0400

    edit clients added

--- src/controllers/clientsController.js
+++ src/controllers/clientsController.js
@@ -1,8 +1,12 @@
+var cliente =
+  "select a.id_client, b.fathers_last_name,b.mothers_last_name,b.first_name, a.cellphone_number,a.mail,a.date_of_birth,a.address,a.registation_date from client a, person b where a.id_person=b.id_person";
+var clienteById =
+  "select a.id_client, b.fathers_last_name,b.mothers_last_name,b.first_name, a.id_client_card,a.cellphone_number,a.mail,a.date_of_birth,a.address,a.longitude_address,a.latitude_address,a.registation_date,a.invoice_name,a.invoice_nit from client a, person b where a.id_person=b.id_person and a.id_client=?";
 const controller = {};
 
 controller.list = (req, res) => {
   req.getConnection((err, conn) => {
-    conn.query('select a.id_client, b.fathers_last_name,b.mothers_last_name,b.first_name, a.cellphone_number,a.mail,a.date_of_birth,a.address,a.registation_date from client a, person b where a.id_person=b.id_person', (err, client) => {
+    conn.query(cliente, (err, client) => {
       if (err) {
         res.json(err);
       } else {
@@ -14,34 +18,38 @@ controller.list = (req, res) => {
     });
   });
 };
-controller.save=(req,res)=>{
+controller.save = (req, res) => {
   const data = req.body;
-  req.getConnection((err,conn)=>{
-    conn.query('INSERT INTO client SET ?',[data],(err,client)=>{
+  req.getConnection((err, conn) => {
+    conn.query("INSERT INTO client SET ?", [data], (err, client) => {
       res.redirect("clients");
     });
-  })
+  });
 };
-controller.edit=(req,res)=>{
-  const { id }=req.params;
- req.getConnection((err,conn)=>{
-  conn.query('SELECT * FROM client wWHERE id =?',[id],(err,client)=>{
-    res.render('clients',{
-      data: client[0]
+controller.edit = (req, res) => {
+  const { id } = req.params;
+  req.getConnection((err, conn) => {
+    conn.query(clienteById, [id], (err, client) => {
+      //console.log(customer);
+      res.render("clients_edit", {
+        data: client[0]
+      });
     });
   });
- });
-}
+};
 
-controller.update=(req,res)=>{
-  const {id} = req.params;
-  const new_client = req.body;
-  req.getConnection((err,conn)=>{
-    conn.query('UPDATE customer set ? where id = ?',[new_client,id],(err,rows)=>{
-      res.redirect('clients');
+controller.update = (req, res) => {
+  const { id } = req.params;
+  const newCliente = req.body;
+  req.getConnection((err, conn) => {
+    conn.query(
+      "UPDATE client set ? where id_client = ?",
+      [newCliente, id],
+      (err, rows) => {
+        res.redirect("/clientes");
+      }
+    );
   });
-})
-}
+};
 
-
-  module.exports = controller;
+module.exports = controller;
--- src/routes/client.js
+++ src/routes/client.js
@@ -4,5 +4,7 @@ const router = express.Router();
 const clientsController = require("../controllers/clientsController");
 
 router.get("/", clientsController.list);
+router.get("/update/:id", clientsController.edit);
+router.post("/update/:id", clientsController.update);
 
 module.exports = router;
