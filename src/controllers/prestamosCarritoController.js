9d988c6701084097187cf49989710d6df351bbb8
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-03-05 02:45:21 -0400

    count video rental

--- src/controllers/prestamosCarritoController.js
+++ src/controllers/prestamosCarritoController.js
@@ -1,8 +1,12 @@
 var peliculasCarrito =
-  "select a.id_video, b.video_title, c.genre, d.first_name,d.last_name, a.stock from video a, video_title b, genre c, actor d, video_genre e, video_actor f where a.id_video = b.id_video and a.id_video = e.id_video and e.id_genre = c.id_genre and a.id_video = f.id_video and f.id_actor = d.id_actor and status=1 and a.stock>0 group by (a.id_video);";
+  "select a.id_video, b.video_title, c.genre, d.first_name,d.last_name, a.stock, a.unit_cost from video a, video_title b, genre c, actor d, video_genre e, video_actor f where a.id_video = b.id_video and a.id_video = e.id_video and e.id_genre = c.id_genre and a.id_video = f.id_video and f.id_actor = d.id_actor and status=1 and a.stock>0 group by (a.id_video);";
 var peliculasCarritoEliminar = "UPDATE video set status=0 where id_video = ?";
 const controller = {};
 var list = new Array();
+var id_client;
+var id_administrador =1;
+var fecha_inicio;
+var fecha_final;
 // controller.list = (req, res) => {
 //   req.getConnection((err, conn) => {
 //     conn.query("SELECT * FROM video", (err, video) => {
@@ -54,6 +58,21 @@ controller.eliminar = (req, res) => {
       if (err) {
         res.json(err);
       } else {
+        res.redirect("/prestamos/carrito");
+      }
+    });
+  });
+};
+
+controller.factura = (req, res) => {
+  const { nit } = req.body;
+  req.getConnection((err, conn) => {
+    conn.query("select id_client from client where id_client = ?", [nit], (err, client) => {
+      if (err) {
+        res.json(err);
+      } else {
+        console.log(client[0]);
+        id_client=client[0];
         res.redirect("/prestamos/carrito");
       }
     });
