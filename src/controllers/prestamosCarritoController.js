b8c4852a8c084f694fe18a9cb0621e442ea144cc
Ernesto Vilela Montero <ernesto.vilela.montero@gmail.com>
2020-03-08 01:47:45 -0400

    calculos total con errores

--- src/controllers/prestamosCarritoController.js
+++ src/controllers/prestamosCarritoController.js
@@ -1,15 +1,16 @@
 var peliculasCarrito =
   "select a.id_video, b.video_title, c.genre, d.first_name,d.last_name, a.stock, a.unit_cost from video a, video_title b, genre c, actor d, video_genre e, video_actor f where a.id_video = b.id_video and a.id_video = e.id_video and e.id_genre = c.id_genre and a.id_video = f.id_video and f.id_actor = d.id_actor and status=1 and a.stock>0 group by (a.id_video);";
 var peliculasCarritoEliminar = "UPDATE video set status=0 where id_video = ?";
-var dias = "select * from rate;";
+var days = "select * from rate;";
 const controller = {};
-var list = new Array();
+var listCarrito = new Array();
 var idClient;
 var id_administrador =1;
-var fecha_inicio;
-var fecha_final;
 var loan;
- let date_ob = new Date();
+var timeCost=0;
+var cantidadPeliculas;
+var idRate=0;
+let date_ob = new Date();
 let day = ("0" + date_ob.getDate()).slice(-2);
 let month = ("0" + (date_ob.getMonth() + 1)).slice(-2);
 let year = date_ob.getFullYear();
@@ -17,45 +18,17 @@ let hours = date_ob.getHours();
 let minutes = date_ob.getMinutes();
 let seconds = date_ob.getSeconds();
 var current_date = year+'-'+month+'-'+day+' '+hours+':'+minutes+':'+seconds;
-var devolution_date = year+'-'+month+'-'+(day+3)+' '+hours+':'+minutes+':'+seconds;
-// controller.list = (req, res) => {
-//   req.getConnection((err, conn) => {
-//     conn.query("SELECT * FROM video", (err, video) => {
-//       if (err) {
-//         res.json(err);
-//       } else {
-//         //console.log(actor);
-//         res.render("prestamosCarrito", {
-//           data: video
-//         });
-//       }
-//     });
-//   });
-// };
-// controller.add_card = (req, res) => {
-//   const { id } = req.params;
-//   req.getConnection((err, conn) => {
-//     conn.query("SELECT * FROM video WHERE id = ?", [id], (err, video) => {
-//       if (err) {
-//         res.json(err);
-//       } else {
-//         console.log(video);
-//         list.add(video);
-//         res.redirect("/prestamos");
-//       }
-//     });
-//   });
-// };
 controller.listCart = (req, res) => {
   req.getConnection((err, conn) => {
     conn.query(peliculasCarrito, (err, videoCarrito) => {
       if (err) {
         res.json(err);
       } else {
-        conn.query(dias, (err, dias) => {
+        conn.query(days, (err, dias) => {
           if (err) {
             res.json(err);
           } else {
+            cantidadPeliculas=videoCarrito.length;
             res.render("prestamosCarrito", {
               data: videoCarrito,
               data2: dias
@@ -66,20 +39,6 @@ controller.listCart = (req, res) => {
     });
   });
 };
-/*controller.listCart = (req, res) => {
-  req.getConnection((err, conn) => {
-    conn.query(peliculasCarrito, (err, video) => {
-      if (err) {
-        res.json(err);
-      } else {
-        //console.log(video);
-        res.render("prestamosCarrito", {
-          data: video
-        });
-      }
-    });
-  });
-};*/
 
 controller.eliminar = (req, res) => {
   const { id } = req.params;
@@ -98,12 +57,12 @@ controller.factura = (req, res) => {
   loan = req.body.d_loan;
   const nit_number = req.body.nit;
   req.getConnection((err, conn) => {
-    conn.query("select id_client from client where invoice_nit = ?", [nit_number], (err, result,fields) => {
+    conn.query("select id_client from client where invoice_nit = ?", [nit_number], (err, result) => {
       if (err) {
         res.json(err);
       } else {
-        console.log("Cliente id "+fields[0]);
-        idClient=8;
+        console.log("Cliente id "+result[0].id_client);
+        idClient=result[0].id_client;
         insertarRenta(req,res);
        // res.redirect("/prestamos/carrito");
       }
@@ -113,11 +72,18 @@ controller.factura = (req, res) => {
 
 function insertarRenta(req,res){
   var d = new Date();
+  obtenerCostoTiempo(req,res);
+  console.log("Costo Tiempo funcion "+timeCost);
+  console.log("Cantidad peliculas funcion "+cantidadPeliculas);
+  var idDesc =obteneridDescuento();
+  obtenerIdTiempo(req,res);
+  console.log("Rate "+idRate);
+  var total = totalFinal();
   req.getConnection((errn,conn)=>{
-    conn.query("insert into rental (id_rate, id_client, start_date, devolution_date, id_discount, total, id_rental_status, id_administrator) values ("+2+","+idClient+",'"+current_date+"','"+formatDate(sumarDias(d,3))+' '+hours+':'+minutes+':'+seconds+"',"+1+","+15.6+","+1+","+1+");",(err,rental)=>{
-      console.log("Renta "+rental);
-      console.log("Error "+err);
-      console.log("Devolución "+formatDate(sumarDias(d,3))+' '+hours+':'+minutes+':'+seconds);
+    conn.query("insert into rental (id_rate, id_client, start_date, devolution_date, id_discount, total, id_rental_status, id_administrator) values ("+idRate+","+idClient+",'"+current_date+"','"+formatDate(sumarDias(d,3))+' '+hours+':'+minutes+':'+seconds+"',"+idDesc+","+total+","+1+","+1+");",(err,rental)=>{
+      //console.log("Renta "+rental);
+      console.log(err);
+      //console.log("Devolución "+formatDate(sumarDias(d,3))+' '+hours+':'+minutes+':'+seconds);
       res.redirect("/prestamos/carrito");
     });
   });
@@ -141,12 +107,52 @@ function formatDate(date) {
 
   return [year, month, day].join('-');
 }
-function obtenerDescuento(cantidad){
-  if(cantidad<=5){
+function obtenerDescuento(){
+  if(cantidadPeliculas<=5){
     return 0.05;
   }
   else{
     return 0.1;
   }
+}
+function obteneridDescuento(){
+  if(cantidadPeliculas<=5){
+    return 1;
+  }
+  else{
+    return 2;
+  }
+}
+function obtenerCostoTiempo(req,res){
+  req.getConnection((err,conn)=>{
+    conn.query("SELECT cost FROM rate WHERE time_rate = ?",[loan],(error,costos)=>{
+        if(err){
+          res.json(error);
+          console.log("Error costo tiempo "+ error)
+        }
+        else{
+          var timeCost=costos[0].cost;
+         
+        }
+    });
+  });
+}
+function obtenerIdTiempo(req,res){
+  req.getConnection((err,conn)=>{
+    conn.query("SELECT id_rate FROM rate WHERE time_rate = ?",[loan],(err,rates)=>{
+        if(err){
+          res.json(err);
+        }
+        else{
+           var idRate=rates[0].id_rate;
+        }
+    });
+  });
+}
+function totalFinal(){
+  console.log("Cantidad final "+cantidadPeliculas);
+  console.log("Costo tiempo final "+ timeCost);
+  console.log("Descuento final "+obtenerDescuento());
+  return (cantidadPeliculas*timeCost)-(cantidadPeliculas*timeCost*obtenerDescuento());
 }
 module.exports = controller;
